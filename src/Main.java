f4a604ac41bc4f222640b259b24ebe36ad95555e
sakarpey <rndsrb12@gmail.com>
2023-08-29 00:32:03 +0300

    добавил проверки на лишние знаки и цифру 10

--- src/Main.java
+++ src/Main.java
@@ -33,7 +33,7 @@ public class Main {
             case 2:
                 break;
             default:
-                throw new Exception("введенное выражение не соответствует условиям ввода: ковычки, расставленные в выражении не соответсвуют условию или их нет");
+                throw new Exception();
         }
         switch (arrayChar[0]) { //проверка что первый символ ковычка
             case '"':
@@ -58,12 +58,21 @@ public class Main {
             default:
                 throw new Exception("введенное выражение не соответствует условиям ввода: не верный знак оператора - можно использовать только \"+ * - /\" ");
         }
-        String znakAfterOperator = str.substring(IndexSymAfterOperator, IndexSymAfterOperator + 1); //получаем знак после оператора "+ * / -"
-        char[] znakAO = znakAfterOperator.toCharArray();
+        String stringAfterOperator = str.substring(IndexSymAfterOperator); //получаем строку после оператора "+ * / -"
+        char[] stringSymAO = stringAfterOperator.toCharArray(); //делим строку полученную выше на массив символов
 
-        switch (znakAO[0]) { //проверяем знак после оператора
-            case '"':
-            case '1':
+        int znak=0;
+        for (char x:stringSymAO  //в этом цикле проверяем, что в строке после оператора больше нет операторов
+             ) {
+            if (x=='+'||x=='-'||x=='*'||x=='/') znak++;
+            if (x=='"') return;
+        }
+            if (znak>0) throw new Exception("введенное выражение не соответствует условиям ввода:");
+
+      int z=0; int y=0;
+        switch (stringSymAO[0]) { //проверяем символ после оператора
+            case '"': y=1;
+            case '1': z=1;
             case '2':
             case '3':
             case '4':
@@ -77,8 +86,21 @@ public class Main {
                 throw new Exception("введенное выражение не соответствует условиям ввода: знак после оператора может быть только любой из указанных '\"', 1,2,3,4,5,6,7,8,9,10 ");
 
         }
-            String [] arraySplitInput = str.split("[+/*-]");
-            if (znakAO[0] !='"' && arraySplitInput.length>1) throw new Exception("введенное выражение не соответствует условиям ввода: введено более одного знака оператора");
+           if (z==1 && stringSymAO.length>1 && stringSymAO[1]!='0')  throw new Exception("введенное выражение не соответствует условиям ввода: знак после оператора может быть только любой из указанных '\"', 1,2,3,4,5,6,7,8,9,10 ");
+
+
+
+           String exA = "Введенное выражение не соответствует условиям ввода:";
+           String exKov1 = "ковычки, расставленные в выражении не соответсвуют условию или их нет";
+           String exKov2 = "выражение должно начинаться с ковычек";
+           String exOper1 = "не верный знак оператора - можно использовать только \"+ * - /\"";
+           String exOper2 = "в выражении может быть только один оператор";
+           String exAO = "знак после оператора может быть только любой из указанных '\"', 1,2,3,4,5,6,7,8,9,10";
+
+
+
+            }
+
 
 
 //        if (znakAO[0] == 1) {
@@ -91,4 +113,3 @@ public class Main {
     }
 
 
-}
