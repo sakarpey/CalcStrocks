a94140bcd80f560b49a4f44b05cadaf1a7431841
sakarpey <rndsrb12@gmail.com>
2023-09-01 19:44:34 +0300

    доделал методы вычиатние умножение и деление

--- src/Action.java
+++ src/Action.java
@@ -9,6 +9,12 @@ public class Action {
        if (str[1].equals("-")){
            res = vychitanie(str);
        }
+       if (str[1].equals("*")){
+           res = umnoghenie(str);
+       }
+       if (str[1].equals("/")){
+           res = delenie(str);
+       }
 
 
        return res;
@@ -18,27 +24,34 @@ public class Action {
         String result = strings[0] + strings[2];
         int x = result.length();
 
-        if (x>40){
-           String catResult = result.substring(0,40);
 
-            return catResult+"...";
-        }
 
         return result;
 
     }
     public static String vychitanie(String[] strings) {
-        String result = strings[0] + strings[1];
+       String result = strings[0].replaceFirst(strings[2],"");
 
         return result;
     }
     public static String umnoghenie(String[] strings) {
-        String result = strings[0] + strings[1];
+       int mnoghitel = Integer.parseInt(strings[2]);
+        String result = strings[0].repeat(mnoghitel);
+        int x=result.length();
+        if (x>40){
+            String catResult = result.substring(0,40);
+
+            return catResult+"...";
+        }
 
         return result;
     }
     public static String delenie(String[] strings) {
-        String result = strings[0] + strings[1];
+        int x=strings[0].length();
+        int y=Integer.parseInt(strings[2]);
+        int z=x/y;
+        String result = strings[0].substring(0,z);
+
 
         return result;
     }
